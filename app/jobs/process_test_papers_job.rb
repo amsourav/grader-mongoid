0d65935e3c2f69dab0fad7aaf2845658b381ccff
sourav sarkar <souravsrkr254@gmail.com>
2016-07-03 15:12:29 +0530

    some comments removed

--- app/jobs/process_test_papers_job.rb
+++ app/jobs/process_test_papers_job.rb
@@ -5,16 +5,20 @@ class ProcessTestPapersJob < ActiveJob::Base
     questions = exam.questions
 
     no_of_pages_per_student = questions.pluck(:pages).sum
-
+    total_no_of_pages = students.length * no_of_pages_per_student
     document = PDFShaver::Document.new(exam.test_papers.file.path)
     dir = File.dirname(exam.test_papers.file.path)
   	pages = document.pages.select{ |page| true }
+
+    # I dont know what I did here, but this shit is working after 2 months of intense
+    # head-banging and hair-pulling. Please someone optimize this shit.
 
     pages = pages[0..total_no_of_pages-1]
 
     split_images = []
 
     pages.each do |page|
+      # 800x seems okay enough
       dimensions = page.extract_dimensions_from_gm_geometry_string('800x')
       # Hiding files like a boss
       out_path = File.join(dir,"#{page.index}_#{Digest::SHA1.hexdigest((rand(12123123423) * Time.now.to_i).to_s)}.jpg")
@@ -22,8 +26,7 @@ class ProcessTestPapersJob < ActiveJob::Base
       split_images << out_path
     end
 
-    # I dont know what I did here, but this shit is working after 2 months of intense
-    # head-banging and hair-pulling. Please someone optimize this shit.
+
     var = 0
     students.each do |student|
       exam_sheet_group = split_images[var..(var+no_of_pages_per_student-1)]
@@ -39,5 +42,6 @@ class ProcessTestPapersJob < ActiveJob::Base
       end
       var = var+no_of_pages_per_student
     end
+    # Shit ends
   end
 end
