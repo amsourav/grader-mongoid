63a8e33ac5b0989feb967cc100d26e378fd6214d
sourav sarkar <souravsrkr254@gmail.com>
2016-07-08 17:26:27 +0530

    some more minor changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,12 +39,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener_web
 
   # Bullet
-  config.after_initialize do
-    Bullet.enable = true
-     Bullet.alert = true
-     Bullet.bullet_logger = true
-     Bullet.console = true
-     Bullet.rails_logger = true
-     Bullet.add_footer = true
-  end
+  # config.after_initialize do
+  #   Bullet.enable = true
+  #    Bullet.alert = true
+  #    Bullet.bullet_logger = true
+  #    Bullet.console = true
+  #    Bullet.rails_logger = true
+  #    Bullet.add_footer = true
+  # end
 end
